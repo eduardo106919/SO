5797f76dec31dfc1abda759c1b37114828653988
eduardo106919 <a106919@alunos.uminho.pt>
2025-03-05 17:44:25 +0000

    improved exercise 2

--- problem_sets/guide-02/exercise2.c
+++ problem_sets/guide-02/exercise2.c
@@ -3,12 +3,16 @@
 
 int main(void) {
     pid_t identifier = fork();
+    int status = 0;
 
     if (identifier == 0) {
         printf("c | id: %d\n", getpid());
         printf("c | parent id: %d\n", getppid());
         _exit(0);
     } else if (identifier > 0) {
+        // wait for the child
+        wait(&status);
+
         printf("p | id: %d\n", getpid());
         printf("p | parent id: %d\n", getppid());
         printf("p | child id: %d\n", identifier);
